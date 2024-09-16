34444a59df761b6ad246255203b59ee9fb83977d
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-09-16 08:09:43 -0300

    Update Marcadores
    Criação do marcador para linha do tempo

--- Solitaria/app/src/main/java/com/example/solitaria/Sinopse.java
+++ Solitaria/app/src/main/java/com/example/solitaria/Sinopse.java
@@ -2,7 +2,9 @@ package com.example.solitaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sinopse extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class Sinopse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinopse);
+
+        final boolean[] Animacao = {false};
+
+        findViewById(R.id.btnLinhaTempo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Animacao[0] == true) {
+                    Intent telaMain = new Intent(getApplicationContext(), LinhaTempo.class);
+                    startActivity(telaMain);
+                } else {
+                    findViewById(R.id.btnLinhaTempo).animate().translationY(-110f).setDuration(100).start();
+                    Animacao[0] = true;
+                }
+            }
+        });
     }
 }
