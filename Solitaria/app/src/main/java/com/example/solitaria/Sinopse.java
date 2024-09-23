f5d5516f2e8f20026c4871dc5d5e8d7bf739f797
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-09-23 08:28:05 -0300

    Update Linha-do-Tempo.java

--- Solitaria/app/src/main/java/com/example/solitaria/Sinopse.java
+++ Solitaria/app/src/main/java/com/example/solitaria/Sinopse.java
@@ -12,20 +12,5 @@ public class Sinopse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sinopse);
-
-        final boolean[] Animacao = {false};
-
-        findViewById(R.id.btnLinhaTempo).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (Animacao[0] == true) {
-                    Intent telaMain = new Intent(getApplicationContext(), LinhaTempo.class);
-                    startActivity(telaMain);
-                } else {
-                    findViewById(R.id.btnLinhaTempo).animate().translationY(-110f).setDuration(100).start();
-                    Animacao[0] = true;
-                }
-            }
-        });
     }
 }
