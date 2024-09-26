e7445e2f6e7614ec8b0296b7e153d0170db32afc
JHRK <jhrk05@gmail.com>
2024-09-26 12:17:51 -0300

    strings caps

--- Solitaria/app/src/main/java/com/example/solitaria/testecaps.java
+++ Solitaria/app/src/main/java/com/example/solitaria/testecaps.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class testecap extends AppCompatActivity {
+public class testecaps extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_testecap);
+        setContentView(R.layout.activity_testecaps);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
