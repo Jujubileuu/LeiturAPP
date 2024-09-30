6276b6e73da06bb4aaee7dde9898b0e88f54f96a
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-09-30 09:14:27 -0300

    Update Merge-Capitulos

--- Solitaria/app/src/main/java/com/example/solitaria/ParteUmCapitulos.java
+++ Solitaria/app/src/main/java/com/example/solitaria/ParteUmCapitulos.java
@@ -2,13 +2,33 @@ package com.example.solitaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class ParteUmCapitulos extends AppCompatActivity {
+
+    public void trocarConteudo(String idCapitulo) {
+        final int y;
+        if (idCapitulo == "quintal") {
+            y = findViewById(R.id.pt1Quintal).getScrollY();
+            findViewById(R.id.scrollCap).scrollTo(0, y);
+        } else if (idCapitulo == "quartobebe") {
+            y = findViewById(R.id.pt1QuartoBebe).getScrollY();
+            findViewById(R.id.scrollCap).scrollTo(0, y);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parte_um_capitulos);
+
+        Intent telaPerfil = getIntent();
+        String idCapitulo = telaPerfil.getStringExtra("idCapitulo");
+
+        trocarConteudo(idCapitulo);
     }
 }
--- Solitaria/app/src/main/java/com/example/solitaria/Sumario.java
+++ Solitaria/app/src/main/java/com/example/solitaria/Sumario.java
@@ -2,13 +2,37 @@ package com.example.solitaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Sumario extends AppCompatActivity {
+
+    String idCapitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sumario);
+
+        findViewById(R.id.pt1Quintal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaPerfil = new Intent(getApplicationContext(), ParteUmCapitulos.class);
+                idCapitulo = "quintal";
+                telaPerfil.putExtra("idCapitulo", idCapitulo);
+                startActivity(telaPerfil);
+            }
+        });
+
+        findViewById(R.id.pt1QuartoBebe).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaPerfil = new Intent(getApplicationContext(), ParteUmCapitulos.class);
+                idCapitulo = "quartobebe";
+                telaPerfil.putExtra("idCapitulo", idCapitulo);
+                startActivity(telaPerfil);
+            }
+        });
     }
 }
