0000c2ac8c021b8bd3c5cf6acf0f548aee08685f
Christian Augustus <108826449+Jujubileuu@users.noreply.github.com>
2024-09-30 07:50:17 -0300

    Create ParteUmCapitulos.java

---
+++ Solitaria/app/src/main/java/com/example/solitaria/ParteUmCapitulos.java
@@ -0,0 +1,14 @@
+package com.example.solitaria;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParteUmCapitulos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parte_um_capitulos);
+    }
+}
