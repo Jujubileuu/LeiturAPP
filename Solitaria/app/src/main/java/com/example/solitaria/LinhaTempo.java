dc8d11355f220470c731f5bb793f9ab56c763455
JHRK <jhrk05@gmail.com>
2024-10-01 11:19:32 -0300

    protlinhatmepo

--- Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
+++ Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
@@ -3,6 +3,8 @@ package com.example.solitaria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class LinhaTempo extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class LinhaTempo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linha_tempo);
+
+
     }
+
 }
