fee0685f855efed6ec1ba703921a4ac1358cd220
JHRK <jhrk05@gmail.com>
2024-10-07 09:06:20 -0300

    tf

--- Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
+++ Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
@@ -3,16 +3,27 @@ package com.example.solitaria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LinhaTempo extends AppCompatActivity {
+
+    TextView textolinha = findViewById(R.id.textolinhatempo);
+    ImageButton btn1 = findViewById(R.id.linhabtn1);
+    ImageButton btn2 = findViewById(R.id.linhabtn2);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linha_tempo);
-
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText("@testelinha1");
+            }
+        });
 
     }
 
