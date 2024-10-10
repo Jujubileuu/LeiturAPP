35d3a9822433a5b1fcc8ecf8e59887239c4b37d7
JHRK <jhrk05@gmail.com>
2024-10-10 11:46:19 -0300

    s

--- Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
+++ Solitaria/app/src/main/java/com/example/solitaria/LinhaTempo.java
@@ -2,8 +2,10 @@ package com.example.solitaria;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,10 +21,22 @@ public class LinhaTempo extends AppCompatActivity {
 
         TextView textolinha = findViewById(R.id.textolinhatempo);
         ImageView imagemLinha = findViewById(R.id.imagemlinha);
-        ImageButton btn1 = findViewById(R.id.linhabtn1);
-        ImageButton btn2 = findViewById(R.id.linhabtn2);
+        Button sair = findViewById(R.id.sair);
+        Button btn1 = findViewById(R.id.linhabtn1);
+        Button btn2 = findViewById(R.id.linhabtn2);
+        Button btn3 = findViewById(R.id.linhabtn3);
+        Button btn4 = findViewById(R.id.linhabtn4);
+        Button btn5 = findViewById(R.id.linhabtn5);
+        Button btn6 = findViewById(R.id.linhabtn6);
+        Button btn7 = findViewById(R.id.linhabtn7);
 
-
+        sair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent telaMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(telaMain);
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -34,6 +48,42 @@ public class LinhaTempo extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 textolinha.setText(R.string.testelinha2);
+                imagemLinha.setImageResource(R.drawable.noggadig);
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText(R.string.testelinha3);
+                imagemLinha.setImageResource(R.drawable.book);
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText(R.string.testelinha4);
+                imagemLinha.setImageResource(R.drawable.ellipse_2);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText(R.string.testelinha5);
+                imagemLinha.setImageResource(R.drawable.ellipse_2);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText(R.string.testelinha6);
+                imagemLinha.setImageResource(R.drawable.ellipse_2);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textolinha.setText(R.string.testelinha7);
+                imagemLinha.setImageResource(R.drawable.ellipse_2);
             }
         });
 
