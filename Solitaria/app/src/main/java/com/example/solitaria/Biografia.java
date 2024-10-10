a2721355a9f25c0a8d5384fca34c474a70eb1982
Jujubileuu <108826449+Jujubileuu@users.noreply.github.com>
2024-10-10 11:37:59 -0300

    Update Marcadores

--- Solitaria/app/src/main/java/com/example/solitaria/Biografia.java
+++ Solitaria/app/src/main/java/com/example/solitaria/Biografia.java
@@ -14,10 +14,64 @@ public class Biografia extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biografia);
 
+        final boolean[] Animacao = {false, false, false, false};
+
+//        findViewById(R.id.btnLinhaTempo).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (Animacao[0]) {
+//                    Intent telaLinhaTempo = new Intent(getApplicationContext(), LinhaTempo.class);
+//                    startActivity(telaLinhaTempo);
+//                } else {
+//                    findViewById(R.id.btnLinhaTempo).animate().translationY(-105f).setDuration(100).start();
+//                    Animacao[0] = true;
+//                }
+//            }
+//        });
+
+        findViewById(R.id.btnSumario).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Animacao[1]) {
+                    Intent telaSumario = new Intent(getApplicationContext(), Sumario.class);
+                    startActivity(telaSumario);
+                } else {
+                    findViewById(R.id.btnSumario).animate().translationY(-110f).setDuration(100).start();
+                    Animacao[1] = true;
+                }
+            }
+        });
+
+        findViewById(R.id.btnGaleria).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Animacao[2]) {
+                    Intent telaGaleria = new Intent(getApplicationContext(), Galeria.class);
+                    startActivity(telaGaleria);
+                } else {
+                    findViewById(R.id.btnGaleria).animate().translationY(-105f).setDuration(100).start();
+                    Animacao[2] = true;
+                }
+            }
+        });
+
+//        findViewById(R.id.btnAutora).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (Animacao[3]) {
+//                    Intent telaAutora = new Intent(getApplicationContext(), Autora.class);
+//                    startActivity(telaAutora);
+//                } else {
+//                    findViewById(R.id.btnAutora).animate().translationY(-110f).setDuration(100).start();
+//                    Animacao[3] = true;
+//                }
+//            }
+//        });
+
         findViewById(R.id.imgAutora).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                urlListener("http://superuser.com/");
+                urlListener("http://www.letras.ufmg.br/literafro/autoras/1159-eliana-alves-cruz");
             }
         });
     }
